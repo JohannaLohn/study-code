6063c619af00df1e871501bdf19641473336449d
JohannaLohn <johanna.lohn31@gmail.com>
2026-04-27 10:23:59 +0200

    drink builder

--- courses/coding-in-C++/Lab_2/MySolution/DrinkBuilder.cpp
+++ courses/coding-in-C++/Lab_2/MySolution/DrinkBuilder.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <string>
 
-class builder
-{
+class drinkBuilder
+{    
+    private:
+        std::string name 
+        int sugar
+        int temperature
+        bool withMilk
+
+    public:
+        drinkBuilder& setName (std::str name){
+            this->name = namespace name;
+            return this*         
+        }
+        drinkBuilder& setSugar (int sugar){
+            this->sugar = sugar;
+            return this*
+        }
+        drinkBuilder& setTemperature (int temperature){
+            this->temperature = temperature;
+            return this*
+        }
+        drinkBuilder& setWithmilk (bool withMilk){
+            this->withMilk = withMilk;
+            return this*
+        }
+}
+
+int main(){
     
+    drinkBuilder builder;
+
+    builder.setName("Tea")
+    .setSugar(2)
+    .setTemperature(65)
+    .setWithMilk(true)
+    .print();
+
+    retrun (0);
+
 }
